de6d56f3f5866dae7124516759f9bbe50cab4278
jstenske <joseph-stenske@uiowa.edu>
2021-03-29 22:34:11 -0500

    Added cost estimate to py file and estimate file.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -15,9 +15,28 @@ def mike():
 def about():
     return render_template('about.html')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['GET'])
 def estimate():
     return render_template('estimate.html')
 
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        num1 = int(request.form['numOne'])
+        num2 = int(request.form['numTwo'])
+        tank_top = 3.14*(num1*num1)
+        tank_sides = 2*(3.14*(num1*num2))
+        total_area = tank_top + tank_sides
+        total_area_sqft = total_area/144
+        mat_cost = total_area_sqft*25
+        labor_cost = total_area_sqft*15
+        total_cost = mat_cost + labor_cost
+        print(total_cost)
+    return render_template('estimate.html', myValue=total_cost)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
